d6c98b00fc76169b2c183fca9b8279d12a055cdc
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-10-22 18:50:56 +0530

    before a solution of user_id

--- routes/userSchema.js
+++ routes/userSchema.js
@@ -2,21 +2,44 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    id: String,
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
     name: String,
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     gender: String,
     age: Number,
     country: String
-})
+});
 
 const messageSchema = new Schema({
-    sender: String,
-    message: String,
-    receiver: String
-})
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'senddata',  // references the user schema
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'senddata',  // references the user schema
+        required: true
+    }
+});
 
 const senddata = mongoose.model('senddata', userSchema);
 const messagedata = mongoose.model('messages', messageSchema);
+
 module.exports = { senddata, messagedata };
